49be63a215b7a1da384c4d380c232bf7980e3618
Tomasz Wodzynski <t910755@gmail.com>
2020-11-10 22:04:49 +0100

    feat: add service

---
+++ restApiDemo.Service/PersonService.cs
@@ -0,0 +1,48 @@
+﻿using restApiDemo.Data.Interfaces;
+using restApiDemo.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace restApiDemo.Service
+{
+    public class PersonService : IPerson
+    {
+        public static List<Person> People = new List<Person>();
+        public static int PersonId = 1;
+
+        public void Add(Person person)
+        {
+            person.Id = PersonId++;
+            People.Add(person);
+        }
+
+        public IEnumerable<Person> GetAll()
+        {
+            return People;
+        }
+
+        public Person GetById(int id)
+        {
+            return People
+                .Where(person => person.Id == id)
+                .FirstOrDefault();
+        }
+
+        public void Update(Person person, int id)
+        {
+            var personIndex = People.FindIndex(person => person.Id == id);
+            var currentPerson = People.Where(person => person.Id == id).FirstOrDefault();
+            person.Id = currentPerson.Id;
+
+            People[personIndex] = person;
+        }
+
+        public void Delete(int id)
+        {
+            var personIndex = People.FindIndex(person => person.Id == id);
+            People.RemoveAt(personIndex);
+        }
+    }
+}
