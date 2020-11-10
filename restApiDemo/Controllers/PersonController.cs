cc376e8df3921f115761e5c8efca7331f6013fa0
Tomasz Wodzynski <t910755@gmail.com>
2020-11-10 22:05:59 +0100

    feat: add api project

---
+++ restApiDemo/Controllers/PersonController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using restApiDemo.Data.Interfaces;
+using restApiDemo.Data.Models;
+
+namespace restApiDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class PersonController : ControllerBase  
+    {
+        private readonly IPerson _personService;
+        //2019-01-06T17:16:40
+
+
+        public PersonController(IPerson personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpPost]
+        public void Add(Person model)
+        {
+            var person = GetPersonModel(model);
+
+            _personService.Add(person);
+        }
+
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return _personService.GetAll();
+        }
+
+        [HttpGet]
+        public Person GetById(int id)
+        {
+            return _personService.GetById(id);
+        }
+
+        [HttpPut]
+        public void Update(Person model, int id)
+        {
+            var check = _personService.GetAll().Count();
+
+            if (id > check)
+            {
+                return;
+            }
+
+            var person = GetPersonModel(model);
+
+            _personService.Update(person, id);
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            _personService.Delete(id);
+        }
+
+        private static Person GetPersonModel(Person model)
+        {
+            return new Person
+            {
+                BirthYear = Convert.ToDateTime(model.BirthYear),
+                Surname = model.Surname,
+                Name = model.Name,
+                Town = model.Town
+            };
+
+        }
+    }
+}
