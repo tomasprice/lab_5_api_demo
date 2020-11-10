3cb6301910e2091c01937499e7c87e7230e9295a
Tomasz Wodzynski <t910755@gmail.com>
2020-11-10 22:04:15 +0100

    feat: add service person model

---
+++ restApiDemo.Data/Interfaces/IPerson.cs
@@ -0,0 +1,16 @@
+﻿using restApiDemo.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace restApiDemo.Data.Interfaces
+{
+    public interface IPerson
+    {
+        IEnumerable<Person> GetAll();
+        Person GetById(int id);
+        void Add(Person person);
+        void Update(Person person, int id);
+        void Delete(int id);
+    }
+}
---
+++ restApiDemo.Data/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace restApiDemo.Data.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthYear { get; set; }
+        public string Town { get; set; }
+    }
+}
