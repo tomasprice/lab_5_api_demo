7e06204a12818b9c098b6680067d2a54179a8e4c
Tomasz Wodzynski <t910755@gmail.com>
2020-11-11 10:26:00 +0100

    feat: add animal model and interface

---
+++ restApiDemo.Data/Interfaces/IAnimal.cs
@@ -0,0 +1,14 @@
+﻿using restApiDemo.Data.Models;
+using System.Collections.Generic;
+
+namespace restApiDemo.Data.Interfaces
+{
+    public interface IAnimal
+    {
+        void Add(Animal animal);
+        void Update(Animal animal, int id);
+        void Delete(int id);
+        Animal GetById(int id);
+        IEnumerable<Animal> GetAll();
+    }
+}
---
+++ restApiDemo.Data/Models/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace restApiDemo.Data.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Weight { get; set; }
+        public bool Carnivorous { get; set; }
+        public DateTime[] CaptureDates { get; set; }
+        public string[] PlacesOfOccurrence { get; set; }
+    }
+}
