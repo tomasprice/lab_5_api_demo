3ea04a64a5a7b297b663b1a919cd6ee2c96fa50f
Tomasz Wodzynski <t910755@gmail.com>
2020-11-28 17:56:27 +0100

    feat(service): add filter methods, add demo data

--- restApiDemo/Controllers/PersonController.cs
+++ restApiDemo/Controllers/PersonController.cs
@@ -21,17 +21,112 @@ namespace restApiDemo.Controllers
         }
 
         [HttpPost]
-        public void Add(Person model)
+        public void Add(Person model,
+            bool townRestrict,
+            bool demoData)
         {
+            if (demoData)
+            {
+                var personList = new List<Person>
+            {
+                { new Person {
+                    Name = "Tomasz",
+                    Surname = "Test",
+                    Town = "Warszawa",
+                    BirthYear = Convert.ToDateTime("1990-01-06T17:16:40")}
+                },
+                { new Person {
+                    Name = "tomasz",
+                    Surname = "Test",
+                    Town = "Łódź",
+                    BirthYear = Convert.ToDateTime("2000-01-06T17:16:40")}
+                },
+                { new Person {
+                    Name = "ToMaSz",
+                    Surname = "Test",
+                    Town = "Gdańsk",
+                    BirthYear = Convert.ToDateTime("1998-01-06T17:16:40")}
+                },
+                { new Person {
+                    Name = "MichAł",
+                    Surname = "Test",
+                    Town = "Siedlce",
+                    BirthYear = Convert.ToDateTime("2019-01-06T17:16:40")}
+                },
+                { new Person {
+                    Name = "MICHał",
+                    Surname = "Test",
+                    Town = "Ełk",
+                    BirthYear = Convert.ToDateTime("1998-01-06T17:16:40")}
+                },
+                { new Person {
+                    Name = "Michał",
+                    Surname = "Test",
+                    Town = "Biała Podlaska",
+                    BirthYear = Convert.ToDateTime("1998-01-06T17:16:40")}
+                },
+                { new Person {
+                    Name = "Jan",
+                    Surname = "Test",
+                    Town = "Gdynia",
+                    BirthYear = Convert.ToDateTime("1990-01-06T17:16:40")}
+                },
+                { new Person {
+                    Name = "Ala",
+                    Surname = "Test",
+                    Town = "Gdynia",
+                    BirthYear = Convert.ToDateTime("1990-01-06T17:16:40")}
+                },
+            };
+
+                foreach (var personDemo in personList)
+                {
+                    _personService.Add(personDemo);
+                }
+
+                return;
+            }
+
             var person = GetPersonModel(model);
 
-            _personService.Add(person);
+            if (townRestrict)
+            {
+                var townAdded = GetPeopleFromTownCount(person.Town);
+
+                if (townAdded == 0)
+                {
+                    return;
+                }
+            }
+            _personService.Add(person);           
         }
 
         [HttpGet]
-        public IEnumerable<Person> Get()
+        public IEnumerable<Person> Get(
+            string name,
+            int birthYear,
+            string town,
+            bool matchCase,
+            bool letterCaseSensitivity)
         {
-            return _personService.GetAll();
+            var tempList = _personService.GetAll().ToList();
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                tempList = GetByName(tempList, name, matchCase, letterCaseSensitivity).ToList();
+            }
+
+            if (birthYear.ToString().Length >= 2 && birthYear.ToString().Length <= 4 && birthYear != -1)
+            {
+                tempList = GetByBirthYear(tempList, birthYear).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(town))
+            {
+                tempList = GetByTown(tempList, town, matchCase, letterCaseSensitivity).ToList();
+            }
+
+            return tempList;
         }
 
         [HttpGet("{id}")]
@@ -61,6 +156,66 @@ namespace restApiDemo.Controllers
             _personService.Delete(id);
         }
 
+
+        // Custom filtering
+        private IEnumerable<Person> GetByName(List<Person> personList, string name,
+            bool matchCase,
+            bool letterCaseSensitivity)
+        {
+            if (matchCase)
+            {
+                if (letterCaseSensitivity)
+                {
+                    return personList
+                        .Where(person => person.Name == name);
+                }
+
+                return personList
+                    .Where(person => person.Name.ToLower() == name.ToLower());
+            }
+
+            if (letterCaseSensitivity)
+            {
+                return personList
+                   .Where(person => person.Name.Contains(name));
+            }
+
+            return personList
+                .Where(person => person.Name.ToLower().Contains(name.ToLower()));
+        }
+
+        private IEnumerable<Person> GetByBirthYear(List<Person> personList, int query)
+        {
+            return personList
+                .Where(person => person.BirthYear.Year == query);
+        }
+
+        private IEnumerable<Person> GetByTown(List<Person> personList, string town,
+          bool matchCase,
+          bool letterCaseSensitivity)
+        {
+            if (matchCase)
+            {
+                if (letterCaseSensitivity)
+                {
+                    return personList
+                        .Where(person => person.Town == town);
+                }
+
+                return personList
+                    .Where(person => person.Town.ToLower() == town.ToLower());
+            }
+
+            if (letterCaseSensitivity)
+            {
+                return personList
+                   .Where(person => person.Town.Contains(town));
+            }
+
+            return personList
+                .Where(person => person.Town.ToLower().Contains(town.ToLower()));
+        }
+
         private static Person GetPersonModel(Person model)
         {
             return new Person
@@ -71,6 +226,13 @@ namespace restApiDemo.Controllers
                 Town = model.Town
             };
 
+        }
+
+        private int GetPeopleFromTownCount(string town)
+        {
+            return _personService.GetAll()
+                .Where(person => person.Town == town)
+                .Count();
         }
     }
 }
